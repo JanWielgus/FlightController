ae6944b763d6f40558578faecdb87673bfad7bd2
Janek <jan.wielgus12@op.pl>
2018-02-19 19:42:21 +0100

    Code side of the yesterday's commit
    All changes in drone control panel (Qt)
    - Added packet serial for Qt with "komunikacja" files from CanSat project
    - Timer to send parameters automaticly every second if was any changes
    - Sending parameters to pilot (without communiaction protocol implementation in "komunikacja")
    - Part of connecting with the pilot

--- FlightControllerControlPanel_v1/mainwindow.cpp
+++ FlightControllerControlPanel_v1/mainwindow.cpp
@@ -1,18 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(timerPIDaction()));
+    timer->start(1000);
+
+    komun.arduino = new QSerialPort(this);
+    komun.pSerial = new PacketSerial;
+    komun.buforT = new uint8_t[komun.pSerial->MAX_SEND_SIZE];
+
+    komun.initialize();
+
+    // Nie dziala: QObject::connect(komun.arduino, SIGNAL(destroyed(QObject*)), this, SLOT(destroySerial())); // chodzi o rozlaczanie gdy wyjety kabel
+    QObject::connect(komun.arduino, SIGNAL(readyRead()), this, SLOT(readSerial()));
 }
 
 MainWindow::~MainWindow()
 {
+    if (komun.arduino->isOpen())
+    {
+        komun.arduino->close(); // Close the serial port if it's open.
+    }
+
     delete ui;
 }
+
+
+void MainWindow::readSerial()
+{
+    if (ui->connectionStateGS_label->text() == connectedText)
+    {
+        komun.odbierz();
+        qDebug() << "Odebrano jakies dane\n";
+    }
+}
+
+
+void MainWindow::timerPIDaction()
+{
+    // Jeśli zmiana w parametrach pid i jest polączony
+    if (needToSendPID && ui->writeParamsAuto_checkBox->isChecked() && ui->connectionStateGS_label->text() == connectedText)
+    {
+        // Wyślij parametry do pilota
+        komun.wyslij(komun.KOMUN_RAMKA_PC_PARAMETERS_TYPE);
+
+        needToSendPID = false;
+    }
+}
+
 
 void MainWindow::on_PID_tuning_checkBox_clicked(bool checked)
 {
@@ -41,4 +86,106 @@ void MainWindow::on_checkBox_clicked(bool checked)
 void MainWindow::on_writeParamsAuto_checkBox_clicked(bool checked)
 {
     ui->writeParams_pushButton->setEnabled(!checked);
+}
+
+void MainWindow::on_searchGS_pushButton_clicked()
+{
+    ui->listOfDevicesGS_comboBox->clear();
+
+    bool arduino_is_available = false;
+
+    //  For each available serial port
+    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts())
+    {
+        //  check if the serialport has both a product identifier and a vendor identifier
+        if(serialPortInfo.hasProductIdentifier() && serialPortInfo.hasVendorIdentifier())
+        {
+            port_name = serialPortInfo.portName();
+            ui->listOfDevicesGS_comboBox->addItem(port_name);
+
+            //  check if the product ID and the vendor ID match those of the arduino uno
+            if((serialPortInfo.productIdentifier() == komun.arduino_uno_product_id) && (serialPortInfo.vendorIdentifier() == komun.arduino_uno_vendor_id))
+            {
+                arduino_is_available = true; //    arduino uno is available on this port
+                ui->gs_arduino_state_label->setText(foundText);
+            }
+        }
+    }
+
+    if (arduino_is_available == false)
+        ui->gs_arduino_state_label->setText(notFoundText);
+}
+
+void MainWindow::on_connectGS_pushButton_clicked()
+{
+    // If arduino is not connected, then you can search
+    if (ui->connectionStateGS_label->text() == notConnectedText)
+    {
+        // If something was selected on list of devices
+        if (ui->listOfDevicesGS_comboBox->currentText() != "")
+        {
+            qDebug() << "Found the arduino port...\n";
+            komun.arduino->setPortName(port_name);
+            komun.arduino->open(QSerialPort::ReadWrite);
+            komun.arduino->setBaudRate(QSerialPort::Baud9600);
+            komun.arduino->setDataBits(QSerialPort::Data8);
+            komun.arduino->setFlowControl(QSerialPort::NoFlowControl);
+            komun.arduino->setParity(QSerialPort::NoParity);
+            komun.arduino->setStopBits(QSerialPort::OneStop);
+
+            ui->listOfDevicesGS_comboBox->clear();
+            ui->gs_arduino_state_label->setText(notFoundText);
+            ui->connectionStateGS_label->setText(connectedText);
+            ui->GS_ConnectionStatus_label->setText(connectedText); // main toolbar
+            ui->connectGS_pushButton->setDisabled(true);
+        }
+        else
+        {
+            qDebug() << "You don't have any devices to connect!\n";
+            ui->GS_ConnectionStatus_label->setText(noConnectionText);
+        }
+    }
+}
+
+void MainWindow::on_endConnectGS_pushButton_clicked()
+{
+    // If is connected and could be closed
+    if (ui->connectionStateGS_label->text() == connectedText)
+    {
+        if (komun.arduino->isOpen())
+        {
+            komun.arduino->close(); // Close the serial port if it's open.
+        }
+
+        ui->connectionStateGS_label->setText(notConnectedText);
+        ui->GS_ConnectionStatus_label->setText(noConnectionText);
+        ui->connectGS_pushButton->setDisabled(false);
+    }
+}
+
+
+void MainWindow::on_writeParams_pushButton_clicked()
+{
+    // Wyślij parametry do pilota
+    komun.wyslij(komun.KOMUN_RAMKA_PC_PARAMETERS_TYPE);
+}
+
+void MainWindow::on_P_PID_spinBox_valueChanged(double arg1)
+{
+    needToSendPID = true;
+}
+
+void MainWindow::on_I_PID_spinBox_valueChanged(double arg1)
+{
+    needToSendPID = true;
+}
+
+void MainWindow::on_D_PID_spinBox_valueChanged(double arg1)
+{
+    needToSendPID = true;
+}
+
+void MainWindow::on_Imax_PID_spinBox_valueChanged(double arg1)
+{
+    needToSendPID = true;
 }
--- FlightControllerControlPanel_v1/mainwindow.h
+++ FlightControllerControlPanel_v1/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "komunikacja.h"
+#include <QString>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -22,8 +25,38 @@ private slots:
 
     void on_writeParamsAuto_checkBox_clicked(bool checked);
 
+    void readSerial();
+
+    void on_searchGS_pushButton_clicked();
+
+    void on_connectGS_pushButton_clicked();
+
+    void on_endConnectGS_pushButton_clicked();
+
+    void timerPIDaction();
+    void on_writeParams_pushButton_clicked();
+
+    void on_P_PID_spinBox_valueChanged(double arg1);
+
+    void on_I_PID_spinBox_valueChanged(double arg1);
+
+    void on_D_PID_spinBox_valueChanged(double arg1);
+
+    void on_Imax_PID_spinBox_valueChanged(double arg1);
+
 private:
     Ui::MainWindow *ui;
+    QTimer* timer;
+
+    QString port_name;
+
+    bool needToSendPID = false;
+
+    const QString notConnectedText = "Not connected";
+    const QString connectedText = "Connected";
+    const QString notFoundText = "Not found";
+    const QString foundText = "Found";
+    const QString noConnectionText = "No connection";
 };
 
 #endif // MAINWINDOW_H
